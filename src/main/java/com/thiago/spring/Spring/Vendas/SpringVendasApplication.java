4c53bbaee70878bb72bc0ac142dab60c40ed0b99
Thiago Leal <thiago.lealx@gmail.com>
2021-12-09 22:28:13 -0300

    secao 3

---
+++ src/main/java/com/thiago/spring/Spring/Vendas/SpringVendasApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.spring.Spring.Vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/spring/Spring/Vendas/SpringVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.spring.Spring.Vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
