816fc60210ed61a70216c893566ee5a7ef128469
Thiago Leal <thiago.lealx@gmail.com>
2021-12-10 01:01:48 -0300

    secao 3, aula 17

--- src/main/java/com/thiago/spring/Spring/Vendas/SpringVendasApplication.java
+++ src/main/java/com/thiago/spring/Spring/Vendas/SpringVendasApplication.java
@@ -1,10 +1,29 @@
 package com.thiago.spring.Spring.Vendas;
 
+import com.thiago.spring.Spring.Vendas.domain.entity.Cliente;
+import com.thiago.spring.Spring.Vendas.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringVendasApplication {
+
+	@Bean
+	public CommandLineRunner init (@Autowired Clientes clientes){
+		return args -> {
+			clientes.salvar(new Cliente("Thiago"));
+			clientes.salvar(new Cliente("Outro cliente"));
+
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+		};
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringVendasApplication.class, args);
