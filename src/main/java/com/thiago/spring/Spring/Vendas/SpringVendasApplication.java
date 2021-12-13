da87050899be6e9a1d8f6f4bd907fb2e908dec20
thiagoleal <thiago.lealx@gmail.com>
2021-12-13 13:56:16 -0300

    modulo 3/;parte spring data

--- src/main/java/com/thiago/spring/Spring/Vendas/SpringVendasApplication.java
+++ src/main/java/com/thiago/spring/Spring/Vendas/SpringVendasApplication.java
@@ -17,29 +17,29 @@ public class SpringVendasApplication {
 	public CommandLineRunner init (@Autowired Clientes clientes){
 		return args -> {
 			System.out.println("Salvando clientes");
-			clientes.salvar(new Cliente("Thiago"));
-			clientes.salvar(new Cliente("Outro cliente"));
+			clientes.save(new Cliente("Thiago"));
+			clientes.save(new Cliente("Outro cliente"));
 
-			List<Cliente> todosClientes = clientes.obterTodos();
+			List<Cliente> todosClientes = clientes.findAll();
 			todosClientes.forEach(System.out::println);
 
 			System.out.println("Atualizando clientes");
 			todosClientes.forEach(c ->{
 				c.setNome(c.getNome() + " atualizado.");
-				clientes.atualizar(c);
+				clientes.save(c);
 			});
-			todosClientes = clientes.obterTodos();
+			todosClientes = clientes.findAll();
 			todosClientes.forEach(System.out::println);
 
 			System.out.println("Buscando clientes");
-			clientes.buscarPorNome("Thi").forEach(System.out::println);
+			clientes.findByNomeLike("Thi").forEach(System.out::println);
 
 			System.out.println("Deletando clientes");
-			clientes.obterTodos().forEach(c -> {
-				clientes.deletar(c);
+			clientes.findAll().forEach(c -> {
+				clientes.delete(c);
 			});
 
-			todosClientes = clientes.obterTodos();
+			todosClientes = clientes.findAll();
 			if(todosClientes.isEmpty()){
 				System.out.println("Nenhum cliente encontrado");
 			}else {
