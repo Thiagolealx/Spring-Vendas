1ee8c4949368213eba2b223534e51d0a766f76ed
thiagoleal <thiago.lealx@gmail.com>
2021-12-13 10:23:28 -0300

    Finalizando o JDBC

--- src/main/java/com/thiago/spring/Spring/Vendas/SpringVendasApplication.java
+++ src/main/java/com/thiago/spring/Spring/Vendas/SpringVendasApplication.java
@@ -16,17 +16,45 @@ public class SpringVendasApplication {
 	@Bean
 	public CommandLineRunner init (@Autowired Clientes clientes){
 		return args -> {
+			System.out.println("Salvando clientes");
 			clientes.salvar(new Cliente("Thiago"));
 			clientes.salvar(new Cliente("Outro cliente"));
 
 			List<Cliente> todosClientes = clientes.obterTodos();
 			todosClientes.forEach(System.out::println);
+
+			System.out.println("Atualizando clientes");
+			todosClientes.forEach(c ->{
+				c.setNome(c.getNome() + " atualizado.");
+				clientes.atualizar(c);
+			});
+			todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+
+			System.out.println("Buscando clientes");
+			clientes.buscarPorNome("Thi").forEach(System.out::println);
+
+			System.out.println("Deletando clientes");
+			clientes.obterTodos().forEach(c -> {
+				clientes.deletar(c);
+			});
+
+			todosClientes = clientes.obterTodos();
+			if(todosClientes.isEmpty()){
+				System.out.println("Nenhum cliente encontrado");
+			}else {
+				todosClientes.forEach(System.out::println);
+			}
+
+
 		};
 	}
 
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringVendasApplication.class, args);
 	}
 
 }
+// teste
