e9ad1193aea149145f9a9d996adcdfa0b12a2b9b
thiagoleal <thiago.lealx@gmail.com>
2022-01-17 13:49:00 -0300

    modulo7 e 8

---
+++ src/main/java/io/github/thiagolealx/domain/enums/StatusPedido.java
@@ -0,0 +1,8 @@
+package io.github.thiagolealx.domain.enums;
+
+public enum StatusPedido {
+
+    REALIZADO,
+    CANCELADO;
+
+}
---
+++ src/main/java/io/github/thiagolealx/exception/RegraNegocioException.java
@@ -0,0 +1,8 @@
+package io.github.thiagolealx.exception;
+
+public class RegraNegocioException extends RuntimeException {
+
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
